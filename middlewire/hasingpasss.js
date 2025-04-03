c5a7f0d68a582a0f32a7fe907d5af26c54d977a4
Nitishrai1 <nitish.12208500@lpu.in>
2025-04-03 11:39:47 +0530

    error-fix

--- middlewire/hasingpasss.js
+++ middlewire/hasingpasss.js
@@ -1,14 +1,12 @@
-
 const bcrypt = require("bcrypt");
-export const hashedPassword=async(plainpassword)=>{
-    const saltround=10;
-    const hashedpassword=await bcrypt.hash(plainpassword,saltround);
-    return hashedPassword;
-}
 
+export const hashedPassword = async (plainpassword) => {
+    const saltround = 10;
+    const hashedpassword = await bcrypt.hash(plainpassword, saltround);
+    return hashedpassword;
+};
 
-export const comparePass=async(plainpassword,hashedpassword)=>{
-
-    const matched=await bcrypt.compare(plainpassword,hashedPassword);
-    return matched
-}
+export const comparePass = async (plainpassword, hashedpassword) => {
+    const matched = await bcrypt.compare(plainpassword, hashedpassword);
+    return matched;
+};
